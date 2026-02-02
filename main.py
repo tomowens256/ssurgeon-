840d197b2c007a1b1b03ca2986fe8a840f17e483
tomowens256 <tomowens256@gmail.com>
2026-02-02 08:46:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11002,7 +11002,7 @@ class ZebraManager:
         self.instruments_dict = instruments_dict or ZEBRA_INSTRUMENTS
         self.running = False
         self.quarter_manager = RobustQuarterManager()
-        self.signal_processor = SignalProcessor()
+        # self.signal_processor = SignalProcessor()
         
         # Set up logger
         if logger:
